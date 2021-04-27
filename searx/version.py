d7c3b35a59606b6b235fb0adf8d2710b640ea268
Markus Heiser <markus.heiser@darmarit.de>
2021-04-27 13:36:13 +0200

    [version] update searxng version of branch master to 1.1.0-pre
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/version.py
+++ searx/version.py
@@ -1,26 +1,16 @@
-# -*- coding: utf-8 -*-
-'''
-searx is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# lint: pylint
 
-searx is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+"""Version of searx
 
-You should have received a copy of the GNU Affero General Public License
-along with searx. If not, see < http://www.gnu.org/licenses/ >.
+"""
 
-(C) 2013- by Adam Tauber, <asciimoo@gmail.com>
-'''
-
-# version of searx
 VERSION_MAJOR = 1
-VERSION_MINOR = 0
-VERSION_BUILD = 0
+VERSION_MINOR = 1
+VERSION_BUILD = "0-pre"
 
-VERSION_STRING = "{0}.{1}.{2}".format(VERSION_MAJOR,
-                                      VERSION_MINOR,
-                                      VERSION_BUILD)
+VERSION_STRING = "{0}.{1}.{2}".format(
+    VERSION_MAJOR,
+    VERSION_MINOR,
+    VERSION_BUILD
+)
