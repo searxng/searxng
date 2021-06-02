92c8a8829f2e68e7ceb3b4670ebea4c4e6541a7c
Alexandre Flament <alex@al-f.net>
2021-06-02 17:28:17 +0200

    [fix] strip spaces from searx user agent
    h11 (used by httpx) rejects HTTP request with a trailing space in HTTP headers

--- searx/utils.py
+++ searx/utils.py
@@ -45,7 +45,7 @@ def searx_useragent():
     """Return the searx User Agent"""
     return 'searx/{searx_version} {suffix}'.format(
            searx_version=VERSION_STRING,
-           suffix=settings['outgoing']['useragent_suffix'].strip())
+           suffix=settings['outgoing']['useragent_suffix']).strip()
 
 
 def gen_useragent(os=None):
