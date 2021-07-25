e02b5469f0ade144f0c6712d95eb4999a69b66b6
Markus Heiser <markus.heiser@darmarit.de>
2021-07-25 12:52:13 +0200

    [mod] use tests/unit/settings/test_settings.yml in unit tests
    In unit tests settings from
    
        searx/settings.yml
    
    and the user settings from:
    
        unit/settings/test_settings.yml
    
    are used.  In the latter, settings can be activated that are needed in the unit
    test but should not activated by default in production.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

---
+++ tests/unit/__init__.py
@@ -0,0 +1,6 @@
+import os
+from os.path import dirname, sep, abspath
+
+# In unit tests the user settings from unit/settings/test_settings.yml are used.
+os.environ['SEARX_SETTINGS_PATH'] = abspath(
+    dirname(__file__) + sep + 'settings' + sep + 'test_settings.yml')
