059f4157faaf4796964a1c71556f948fdfe3a698
Alexandre Flament <alex@al-f.net>
2021-11-30 19:31:55 +0100

    [mod] simple theme: move initilization of document.searxng to searxng.min.js
    keep searxng.min.js as small as possible

---
+++ searx/static/themes/simple/src/js/main/01_init.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * (C) Copyright Contributors to the SearXNG project.
+ * (C) Copyright Contributors to the searx project (2014 - 2021).
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+(function(w, d) {
+    'use strict';
+
+    // add data- properties
+    var script = d.currentScript  || (function() {
+        var scripts = d.getElementsByTagName('script');
+        return scripts[scripts.length - 1];
+    })();
+
+    // try to detect touch screen
+    w.searxng = {
+        touch: d.getElementsByTagName("html")[0].classList.contains('touch'),
+        method: script.getAttribute('data-method'),
+        autocompleter: script.getAttribute('data-autocompleter') === 'true',
+        search_on_category_select: script.getAttribute('data-search-on-category-select') === 'true',
+        infinite_scroll: script.getAttribute('data-infinite-scroll') === 'true',
+        hotkeys: script.getAttribute('data-hotkeys') === 'true',
+        static_path: script.getAttribute('data-static-path'),
+        translations: JSON.parse(script.getAttribute('data-translations')),
+        theme : {
+            // image that is displayed if load of <img src='...'> failed
+            img_load_error: 'img/img_load_error.svg'
+        },
+    };
+})(window, document);
