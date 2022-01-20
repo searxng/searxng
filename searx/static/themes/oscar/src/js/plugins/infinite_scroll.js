213ce85252ee7ded1112390d2bc6c832431ce36d
Martin Fischer <martin@push-f.com>
2022-01-20 10:07:10 +0100

    [oscar] fix indentation of plugin JS
    This change was split off into a separate commit
    so that the previous commit had a clean diff.

--- searx/static/themes/oscar/src/js/plugins/infinite_scroll.js
+++ searx/static/themes/oscar/src/js/plugins/infinite_scroll.js
@@ -1,66 +1,66 @@
 if (searxng.plugins['searx.plugins.infinite_scroll']) {
-function hasScrollbar() {
-    var root = document.compatMode=='BackCompat'? document.body : document.documentElement;
-    return root.scrollHeight>root.clientHeight;
-}
+    function hasScrollbar() {
+        var root = document.compatMode=='BackCompat'? document.body : document.documentElement;
+        return root.scrollHeight>root.clientHeight;
+    }
 
-function loadNextPage() {
-    var formData = $('#pagination form:last').serialize();
-    if (formData) {
-        $('#pagination').html('<div class="loading-spinner"></div>');
-        $.ajax({
-            type: "POST",
-            url: $('#search_form').prop('action'),
-            data: formData,
-            dataType: 'html',
-            success: function(data) {
-                var body = $(data);
-                $('#pagination').remove();
-                $('#main_results').append('<hr/>');
-                $('#main_results').append(body.find('.result'));
-                $('#main_results').append(body.find('#pagination'));
-                if(!hasScrollbar()) {
-                    loadNextPage();
+    function loadNextPage() {
+        var formData = $('#pagination form:last').serialize();
+        if (formData) {
+            $('#pagination').html('<div class="loading-spinner"></div>');
+            $.ajax({
+                type: "POST",
+                url: $('#search_form').prop('action'),
+                data: formData,
+                dataType: 'html',
+                success: function(data) {
+                    var body = $(data);
+                    $('#pagination').remove();
+                    $('#main_results').append('<hr/>');
+                    $('#main_results').append(body.find('.result'));
+                    $('#main_results').append(body.find('#pagination'));
+                    if(!hasScrollbar()) {
+                        loadNextPage();
+                    }
                 }
-            }
-        });
+            });
+        }
     }
-}
 
-$(document).ready(function() {
-    var win = $(window);
-    if(!hasScrollbar()) {
-        loadNextPage();
-    }
-    win.scroll(function() {
-        $("#pagination button").css("visibility", "hidden");
-        if ($(document).height() - win.height() - win.scrollTop() < 150) {
+    $(document).ready(function() {
+        var win = $(window);
+        if(!hasScrollbar()) {
             loadNextPage();
         }
+        win.scroll(function() {
+            $("#pagination button").css("visibility", "hidden");
+            if ($(document).height() - win.height() - win.scrollTop() < 150) {
+                loadNextPage();
+            }
+        });
     });
-});
 
-const style = document.createElement('style');
-style.textContent = `
-@keyframes rotate-forever {
-    0%   { transform: rotate(0deg) }
-    100% { transform: rotate(360deg) }
-}
-.loading-spinner {
-    animation-duration: 0.75s;
-    animation-iteration-count: infinite;
-    animation-name: rotate-forever;
-    animation-timing-function: linear;
-    height: 30px;
-    width: 30px;
-    border: 8px solid #666;
-    border-right-color: transparent;
-    border-radius: 50% !important;
-    margin: 0 auto;
-}
-#pagination button {
-	visibility: hidden;
-}
-`;
-document.head.append(style);
+    const style = document.createElement('style');
+    style.textContent = `
+    @keyframes rotate-forever {
+        0%   { transform: rotate(0deg) }
+        100% { transform: rotate(360deg) }
+    }
+    .loading-spinner {
+        animation-duration: 0.75s;
+        animation-iteration-count: infinite;
+        animation-name: rotate-forever;
+        animation-timing-function: linear;
+        height: 30px;
+        width: 30px;
+        border: 8px solid #666;
+        border-right-color: transparent;
+        border-radius: 50% !important;
+        margin: 0 auto;
+    }
+    #pagination button {
+        visibility: hidden;
+    }
+    `;
+    document.head.append(style);
 }
--- searx/static/themes/oscar/src/js/plugins/search_on_category_select.js
+++ searx/static/themes/oscar/src/js/plugins/search_on_category_select.js
@@ -1,44 +1,44 @@
 if (searxng.plugins['searx.plugins.search_on_category_select']) {
-$(document).ready(function() {
-    if($('#q').length) {
-        $('#categories label').click(function(e) {
-            $('#categories input[type="checkbox"]').each(function(i, checkbox) {
-                $(checkbox).prop('checked', false);
+    $(document).ready(function() {
+        if($('#q').length) {
+            $('#categories label').click(function(e) {
+                $('#categories input[type="checkbox"]').each(function(i, checkbox) {
+                    $(checkbox).prop('checked', false);
+                });
+                $(document.getElementById($(this).attr("for"))).prop('checked', true);
+                if($('#q').val()) {
+                    if (getHttpRequest() == "GET") {
+                        $('#search_form').attr('action', $('#search_form').serialize());
+                    }
+                    $('#search_form').submit();
+                }
+                return false;
             });
-            $(document.getElementById($(this).attr("for"))).prop('checked', true);
-            if($('#q').val()) {
-                if (getHttpRequest() == "GET") {
-                    $('#search_form').attr('action', $('#search_form').serialize());
+            $('#time-range').change(function(e) {
+                if($('#q').val()) {
+                    if (getHttpRequest() == "GET") {
+                        $('#search_form').attr('action', $('#search_form').serialize());
+                    }
+                    $('#search_form').submit();
                 }
-                $('#search_form').submit();
-            }
-            return false;
-        });
-        $('#time-range').change(function(e) {
-            if($('#q').val()) {
-                if (getHttpRequest() == "GET") {
-                    $('#search_form').attr('action', $('#search_form').serialize());
+            });
+            $('#language').change(function(e) {
+                if($('#q').val()) {
+                    if (getHttpRequest() == "GET") {
+                        $('#search_form').attr('action', $('#search_form').serialize());
+                    }
+                    $('#search_form').submit();
                 }
-                $('#search_form').submit();
-            }
-        });
-        $('#language').change(function(e) {
-            if($('#q').val()) {
-                if (getHttpRequest() == "GET") {
-                    $('#search_form').attr('action', $('#search_form').serialize());
-                }
-                $('#search_form').submit();
-            }
-        });
-    }
-});
+            });
+        }
+    });
 
-function getHttpRequest() {
-    httpRequest = "POST";
-    urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('method')) {
-        httpRequest = urlParams.get('method');
+    function getHttpRequest() {
+        httpRequest = "POST";
+        urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has('method')) {
+            httpRequest = urlParams.get('method');
+        }
+        return httpRequest;
     }
-    return httpRequest;
-}
 }
