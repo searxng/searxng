7f89adc5f39a9b3d2a03d32728010d0a770b89a3
Martin Fischer <martin@push-f.com>
2022-01-24 09:53:30 +0100

    [pyright:strict] searx.shared.shared_abstract

--- searx/shared/shared_abstract.py
+++ searx/shared/shared_abstract.py
@@ -1,20 +1,22 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+# pyright: strict
 from abc import ABC, abstractmethod
+from typing import Optional
 
 
 class SharedDict(ABC):
     @abstractmethod
-    def get_int(self, key):
+    def get_int(self, key: str) -> Optional[int]:
         pass
 
     @abstractmethod
-    def set_int(self, key, value):
+    def set_int(self, key: str, value: int):
         pass
 
     @abstractmethod
-    def get_str(self, key):
+    def get_str(self, key: str) -> Optional[str]:
         pass
 
     @abstractmethod
-    def set_str(self, key, value):
+    def set_str(self, key: str, value: str):
         pass
