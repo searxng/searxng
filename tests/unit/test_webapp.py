b93711b45da7e8d851be5a3d55ebf6fbe5423efd
Martin Fischer <martin@push-f.com>
2022-01-31 11:31:07 +0100

    [help] remove link from about.md title
    Now that about.html extends page_with_header.html
    it already has a link to the start page and removing
    the link makes it easier to extract the page title
    from the Markdown for the following commit.

--- tests/unit/test_webapp.py
+++ tests/unit/test_webapp.py
@@ -176,7 +176,7 @@ class ViewsTestCase(SearxTestCase):
     def test_about(self):
         result = self.app.get('/about')
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'<h1>About <a href="/">searxng</a></h1>', result.data)
+        self.assertIn(b'<h1>About SearXNG</h1>', result.data)
 
     def test_health(self):
         result = self.app.get('/healthz')
