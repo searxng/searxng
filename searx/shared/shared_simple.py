9b26e48ccddbd5ce2d98459add920d6e2c5258cd
Marc Abonce Seguin <marc-abonce@mailbox.org>
2022-04-24 23:52:13 -0500

    add rate limiting per engine

--- searx/shared/shared_simple.py
+++ searx/shared/shared_simple.py
@@ -16,14 +16,28 @@ class SimpleSharedDict(shared_abstract.SharedDict):
     def get_int(self, key: str) -> Optional[int]:
         return self.d.get(key, None)
 
-    def set_int(self, key: str, value: int):
+    def set_int(self, key: str, value: int, expire: Optional[int] = None):
         self.d[key] = value
+        if expire:
+            self._expire(key, expire)
 
     def get_str(self, key: str) -> Optional[str]:
         return self.d.get(key, None)
 
-    def set_str(self, key: str, value: str):
+    def set_str(self, key: str, value: str, expire: Optional[int] = None):
         self.d[key] = value
+        if expire:
+            self._expire(key, expire)
+
+    def _expire(self, key: str, expire: int):
+        t = threading.Timer(expire, lambda k, d: d.pop(k), args=[key, self.d])
+        t.daemon = True
+        t.start()
+
+
+def run_locked(func, *args):
+    # SimpleSharedDict is not actually shared, so no locking needed
+    return func(*args)
 
 
 def schedule(delay, func, *args):
