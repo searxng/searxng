66b77c46c73489ef2b3d3223cd31883d0eb7bcd9
Émilien Devos <contact@emiliendevos.be>
2022-05-10 20:45:59 +0000

    Fix typo

--- searx/plugins/limiter.py
+++ searx/plugins/limiter.py
@@ -3,7 +3,7 @@
 # pyright: basic
 """Some bot protection / rate limitation
 
-To monitore rate limits and protect privacy the IP addresses are getting stored
+To monitor rate limits and protect privacy the IP addresses are getting stored
 with a hash so the limiter plugin knows who to block.  A redis database is
 needed to store the hash values.
 
