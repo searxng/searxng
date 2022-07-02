3e69a98f8017f4e4087f21dd39896f135c6cb569
Markus Heiser <markus.heiser@darmarit.de>
2022-07-02 19:43:14 +0200

    [fix] hostname_replace: modify URLs in the infobox
    Closes: https://github.com/searxng/searxng/issues/1348
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/results/answer.py
+++ searx/results/answer.py
@@ -35,3 +35,30 @@ class Answers(dict):
 
     def add(self, result):
         self[result['answer']] = result
+
+
+def answer_modify_url(modify_url_func, result):
+    """Modify 'url' field in the answer-result.
+
+    :param func modify_url_func: A function that gets one argument; the 'url'
+        field of the ``result`` item.  The function returns the URL to use
+        instead (even the URL is not modified).  To drop the 'url' field from
+        the result the function returns ``None``.
+
+    :param dict result: The result item.
+    """
+
+    if not is_answer(result):
+        return
+
+    url = result.get('url')
+    if not url:
+        return
+
+    _url = modify_url_func(url)
+    if _url is None:
+        # logger.debug("answer: remove url from %s", url)
+        del result['url']
+    elif _url != url:
+        # logger.debug("answer: redirect url %s", _url)
+        result['url'] = _url
