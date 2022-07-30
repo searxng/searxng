bded8750d5b4ae743dd96b3a814a78da66ab4a02
Markus Heiser <markus.heiser@darmarit.de>
2022-07-30 16:13:47 +0200

    [mod] fix minor leftovers from PR #1332
    Related: https://github.com/searxng/searxng/pull/1332
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- utils/morty.sh
+++ utils/morty.sh
@@ -3,10 +3,14 @@
 
 # shellcheck source=utils/lib.sh
 source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
+# shellcheck source=utils/brand.env
+source "${REPO_ROOT}/utils/brand.env"
 
 # ----------------------------------------------------------------------------
 # config
 # ----------------------------------------------------------------------------
+
+PUBLIC_URL="${PUBLIC_URL:-${SEARXNG_URL}}"
 
 MORTY_LISTEN="${MORTY_LISTEN:-127.0.0.1:3000}"
 PUBLIC_URL_PATH_MORTY="${PUBLIC_URL_PATH_MORTY:-/morty/}"
@@ -35,6 +39,9 @@ usage::
 remove all     : drop all components of the morty service
 apache remove  : drop apache site ${APACHE_MORTY_SITE}
 nginx  remove  : drop nginx site ${NGINX_MORTY_SITE}
+
+environment:
+  PUBLIC_URL_MORTY   : ${PUBLIC_URL_MORTY}
 EOF
 
     [[ -n ${1} ]] &&  err_msg "$1"
