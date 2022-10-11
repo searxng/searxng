9933155a2e8d8e2f3dc7280d909c6851b16ba863
Markus Heiser <markus.heiser@darmarit.de>
2022-10-11 11:45:26 +0200

    [fix] update_osm_keys_tags.py: sort JSON dump
    To get meaningful diff, the keys in JSON dump needs to be sorted.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searxng_extra/update/update_osm_keys_tags.py
+++ searxng_extra/update/update_osm_keys_tags.py
@@ -215,4 +215,4 @@ if __name__ == '__main__':
         'tags': optimize_tags(get_tags()),
     }
     with open(get_osm_tags_filename(), 'w', encoding="utf8") as f:
-        json.dump(result, f, indent=4, ensure_ascii=False)
+        json.dump(result, f, indent=4, ensure_ascii=False, sort_keys=True)
