6804ff048d02e302162487f40843391f9018402e
Rudis Muiznieks <rudis@sitosis.com>
2022-12-22 07:49:58 -0600

    Fix: add trailing slash to duckduckgo url
    Close #1854

--- searx/engines/duckduckgo.py
+++ searx/engines/duckduckgo.py
@@ -18,7 +18,7 @@ from searx.network import get
 
 # about
 about = {
-    "website": 'https://lite.duckduckgo.com/lite',
+    "website": 'https://lite.duckduckgo.com/lite/',
     "wikidata_id": 'Q12805',
     "official_api_documentation": 'https://duckduckgo.com/api',
     "use_official_api": False,
@@ -46,7 +46,7 @@ language_aliases = {
 time_range_dict = {'day': 'd', 'week': 'w', 'month': 'm', 'year': 'y'}
 
 # search-url
-url = 'https://lite.duckduckgo.com/lite'
+url = 'https://lite.duckduckgo.com/lite/'
 url_ping = 'https://duckduckgo.com/t/sl_l'
 
 # match query's language to a region code that duckduckgo will accept
