5894af97837e1dc5678e0ea72f93ffe21fb33923
luxtiasco <79135227+luxtiasco@users.noreply.github.com>
2023-02-22 22:32:42 +0800

    更新 webapp.py

--- searx/webapp.py
+++ searx/webapp.py
@@ -983,13 +983,13 @@ function replaceUrlWithFootnote(text) {
 }
 function beautify(text)
 {
-new_text=text
+new_text=text.replaceAll("（","(").replaceAll("）",")")
 for(let i=prompt.url_pair.length;i>=0;--i)
 {
     new_text = new_text.replaceAll("(url"+String(i),"(https://url"+String(i) )
     new_text = new_text.replaceAll("(链接"+String(i),"(https://url"+String(i) )
 }
-  new_text = replaceUrlWithFootnote(new_text.replaceAll("（","(").replaceAll("）",")"))
+  new_text = replaceUrlWithFootnote(new_text)
 
     for(let i=prompt.url_pair.length;i>=0;--i)
     {
