16965f95d682351ec975bb9ffa0fb8803470775b
Joseph Cheung <luxtiasco@outlook.com>
2023-03-01 20:10:09 +0800

    c

--- searx/webapp1.py
+++ searx/webapp1.py
@@ -1751,7 +1751,7 @@ function send_modalchat(elem)
     prompt = prompt +'网页内容：\n'
 
     fulltext.sort((a, b) => {
-        if (a.cosineSimilarity(word,a) < b.cosineSimilarity(word,b)) {
+        if (cosineSimilarity(word,a) < cosineSimilarity(word,b)) {
             return -1
         } else {
             return 1
