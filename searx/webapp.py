17cbad5b01ec6b1f23a649e9dcffec075a7db582
Joseph Cheung <luxtiasco@outlook.com>
2023-03-02 22:16:58 +0800

    c

--- searx/webapp.py
+++ searx/webapp.py
@@ -1202,7 +1202,7 @@ function proxify()
             {
                 let tmp_url = document.querySelector("#fnref\\:"+String(i+1)).href
                 if(!tmp_url||!prompt.url_proxy[tmp_url]) continue;
-                const nowurl = modal_open(prompt.url_proxy[tmp_url]
+                const nowurl = modal_open(prompt.url_proxy[tmp_url])
                 document.querySelector("#fnref\\:"+String(i+1)).onclick = function() { modal_open(nowurl,i+1) };
                 document.querySelector("#fnref\\:"+String(i+1)).removeAttribute('href')
                 document.querySelector("#fnref\\:"+String(i+1)).removeAttribute('id')
