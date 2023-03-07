b081ec0118aa9760aa8fae750d01bf016fbd33c8
Joseph Cheung <luxtiasco@outlook.com>
2023-03-07 13:01:03 +0800

    d

---
+++ searx/engines/tmdb.py
@@ -0,0 +1,28 @@
+from json import loads
+from urllib.parse import urlencode
+SEARCH_URL = "https://api.themoviedb.org/3/search/multi"
+API_KEY = "f6bd687ffa63cd282b6ff2c6877f2669"
+
+def request(query, params):
+    params["url"] = SEARCH_URL + "?api_key=" + API_KEY + "&query=" + urlencode(query)
+    language = params.get("language")
+    if language:
+        params["url"] += "&language=" + language
+    pageno = params.get("pageno")
+    if pageno:
+        params["url"] += "&page=" + str(pageno)
+    return params
+
+def response(resp):
+    results = []
+    data = loads(resp.text)["results"]
+    if data:
+        for item in data:
+            result = {
+                "title": item.get("name"),
+                "url": f"https://www.themoviedb.org/{item.get('media_type')}/{item.get('id')}",
+                "content": item.get("overview"),
+                "publishedDate": item.get("release_date") or item.get("first_air_date"),
+            }
+            results.append(result)
+    return results
