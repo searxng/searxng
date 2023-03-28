4cdecfbfb307c8eb6a6f30ada5ea09624a04f8f1
RecentRichRail <80782821+RecentRichRail@users.noreply.github.com>
2023-03-28 12:02:31 -0400

    Update chatgpt.py

--- searx/engines/chatgpt.py
+++ searx/engines/chatgpt.py
@@ -1,28 +1,16 @@
 import os
-from flask import Flask, jsonify, request
 import openai
 
-app = Flask(__name__)
-
-# retrieve ChatGPT API key from system variable
-API_KEY = os.environ.get("CHATGPT_API_KEY")
-
-# initialize OpenAI API client
-openai.api_key = API_KEY
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 engine_id = "text-davinci-002"
 
-@app.route('/chatgpt', methods=['GET'])
-def chatgpt():
-    query = request.args.get('query')
+def generate_response(prompt):
     response = openai.Completion.create(
         engine=engine_id,
-        prompt=query,
+        prompt=prompt,
         max_tokens=150,
         n=1,
-        stop="\n"
+        stop=None,
+        temperature=0.7
     )
-    chatgpt_response = response.choices[0].text
-    return chatgpt_response
-
-if __name__ == '__main__':
-    app.run()
+    return response.choices[0].text.strip()
