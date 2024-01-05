2b0d412c912b3701c3839e06a3be85f2d1cd444b
Bnyro <bnyro@tutanota.com>
2024-01-05 18:03:07 +0100

    [feat] engine: implementation of stract (beta)

---
+++ searx/engines/stract.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# lint: pylint
+"""Stract is an independent open source search engine. 
+At this state, it's still in beta and hence this implementation will need to be updated once beta ends.
+"""
+
+from json import dumps
+
+about = {
+    "website": "https://stract.com/",
+    "use_official_api": True,
+    "official_api_documentation": "https://stract.com/beta/api/docs/#/search/api",
+    "require_api_key": False,
+    "results": "JSON",
+}
+categories = ['general']
+paging = True
+
+search_url = "https://stract.com/beta/api/search"
+
+
+def request(query, params):
+    params['url'] = search_url
+    params['method'] = "POST"
+    params['headers'] = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    params['data'] = dumps({'query': query, 'page': params['pageno'] - 1})
+
+    return params
+
+
+def response(resp):
+    results = []
+
+    for result in resp.json()["webpages"]:
+        results.append(
+            {
+                'url': result['url'],
+                'title': result['title'],
+                'content': ''.join(fragment['text'] for fragment in result['snippet']['text']['fragments']),
+            }
+        )
+
+    return results
