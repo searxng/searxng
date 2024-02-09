8f81058e71e5f1fb0726661fb2bbc77524e3b8fa
Valentin Riess <valentin.riess@posteo.de>
2024-02-10 00:23:52 +0100

    Fix formatting

--- searx/engines/nixpkgs.py
+++ searx/engines/nixpkgs.py
@@ -27,33 +27,33 @@ Credentials are available here: https://github.com/NixOS/nixos-search/blob/main/
 
 """
 
-from json import loads, dumps
+from json import loads
 from searx.exceptions import SearxEngineAPIException
 
-base_url = 'https://search.nixos.org/backend'
-username = ''
-password = ''
-index = 'latest-42-nixos-unstable'
-search_url = base_url + '/' + index + '/_search'
+base_url = "https://search.nixos.org/backend"
+username = ""
+password = ""
+index = "latest-42-nixos-unstable"
+search_url = base_url + "/" + index + "/_search"
 show_metadata = False
-categories = ['general']
+categories = ["general"]
 
 
 def init():
     if not (username or password):
-        raise ValueError('username and password need to be configured.')
+        raise ValueError("username and password need to be configured.")
 
 
 def request(query, params):
-    search_url = base_url + '/' + index + '/_search'
-    
-    if username and password:
-        params['auth'] = (username, password)
+    search_url = base_url + "/" + index + "/_search"
 
-    params['url'] = search_url
-    params['method'] = 'GET'
-    params['data'] = _build_query(query)
-    params['headers']['Content-Type'] = 'application/json'
+    if username and password:
+        params["auth"] = (username, password)
+
+    params["url"] = search_url
+    params["method"] = "GET"
+    params["data"] = _build_query(query)
+    params["headers"]["Content-Type"] = "application/json"
 
     return params
 
@@ -62,20 +62,24 @@ def response(resp):
     results = []
 
     resp_json = loads(resp.text)
-    if 'error' in resp_json:
-        raise SearxEngineAPIException(resp_json['error'])
+    if "error" in resp_json:
+        raise SearxEngineAPIException(resp_json["error"])
 
-    for result in resp_json['hits']['hits']:
-        r = {key: value if not key.startswith('_') else value for key, value in result['_source'].items()}
-        r['template'] = 'nix-package.html'
-        r['url'] = _get_url(result)
-        r['title'] = result['_source']['package_pname']
-        r['content'] = result['_source']['package_description']
-        r['github_url'] = _position_to_github_url(result['_source']['package_position'])
-        r['codelines'] = _get_codelines(result['_source']['package_pname'])
+    for result in resp_json["hits"]["hits"]:
+        r = {key: value if not key.startswith("_") else value for key, value in result["_source"].items()}
+        r["template"] = "nix-package.html"
+        r["url"] = _get_url(result)
+        r["title"] = result["_source"]["package_pname"]
+        r["content"] = result["_source"]["package_description"]
+        r["github_url"] = _position_to_github_url(result["_source"]["package_position"])
+        r["codelines"] = _get_codelines(result["_source"]["package_pname"])
 
         if show_metadata:
-            r['metadata'] = {'index': result['_index'], 'id': result['_id'], 'score': result['_score']}
+            r["metadata"] = {
+                "index": result["_index"],
+                "id": result["_id"],
+                "score": result["_score"],
+            }
 
         results.append(r)
 
@@ -144,22 +148,22 @@ def _build_query(query: str):
 
 
 def _position_to_github_url(package_position: str):
-    path, line = package_position.split(':')
+    path, line = package_position.split(":")
     return f"https://github.com/NixOS/nixpkgs/blob/master/{path}#L{line}"
 
 
 def _get_url(result: dict):
     try:
-        url = result['_source']['package_homepage'][0]
+        url = result["_source"]["package_homepage"][0]
     except:
         try:
-            position = result['_source']['package_position']
+            position = result["_source"]["package_position"]
             if not position:
                 raise
             else:
                 url = _position_to_github_url(position)
         except:
-            url = 'https://search.nixos.org'
+            url = "https://search.nixos.org"
     return url
 
 
