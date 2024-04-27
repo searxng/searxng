bcab8ec575be90ee27b63213e6198b560ba7aeed
Markus Heiser <markus.heiser@darmarit.de>
2024-04-27 17:02:04 +0200

    [fix] ccc_media engine: filter video formats and ignore audio & SubRip
    CCC media serves several recording formats, to name a few:
    
    - application/x-subrip
    - video/mp4
    - video/webm
    - audio/mpeg
    - audio/opus
    - audio/mpeg
    
    not all of them are suitable for a video frame.  If available we should prefer
    video/mp4 due to its minimal data rates.
    
    Closes: https://github.com/searxng/searxng/issues/3431
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/engines/ccc_media.py
+++ searx/engines/ccc_media.py
@@ -35,8 +35,13 @@ def response(resp):
             publishedDate = parser.parse(item['date'])
 
         iframe_src = None
-        if len(item['recordings']) > 0:
-            iframe_src = item['recordings'][0]['recording_url']
+        for rec in item['recordings']:
+            if rec['mime_type'].startswith('video'):
+                if not iframe_src:
+                    iframe_src = rec['recording_url']
+                elif rec['mime_type'] == 'video/mp4':
+                    # prefer mp4 (minimal data rates)
+                    iframe_src = rec['recording_url']
 
         results.append(
             {
