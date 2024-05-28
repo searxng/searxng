fa8dd95414d0d5ee23be34d5fa55a7cb05584e2f
Markus Heiser <markus.heiser@darmarit.de>
2024-05-28 17:56:54 +0200

    [fix] issues reported by pylint-3.2.2
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/infopage/__init__.py
+++ searx/infopage/__init__.py
@@ -17,6 +17,8 @@ Usage in a Flask app route:
 
 """
 
+from __future__ import annotations
+
 __all__ = ['InfoPage', 'InfoPageSet']
 
 import os
@@ -37,6 +39,16 @@ from ..locales import LOCALE_NAMES
 
 logger = logging.getLogger('searx.infopage')
 _INFO_FOLDER = os.path.abspath(os.path.dirname(__file__))
+INFO_PAGES: 'InfoPageSet'
+
+
+def __getattr__(name):
+    if name == 'INFO_PAGES':
+        global INFO_PAGES  # pylint: disable=global-statement
+        INFO_PAGES = InfoPageSet()
+        return INFO_PAGES
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 
 class InfoPage:
