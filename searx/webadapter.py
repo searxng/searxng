3f4799894474f274d0f37c53e2d353c1381cc6e9
Jafar Farganlooj <86001599+mehr32@users.noreply.github.com>
2024-08-15 09:27:18 +0330

    Reducing the sensitivity threshold of language recognition

--- searx/webadapter.py
+++ searx/webadapter.py
@@ -267,7 +267,7 @@ def get_search_query_from_webapp(
     selected_locale = query_lang
 
     if query_lang == 'auto':
-        query_lang = detect_language(query, threshold=0.8, only_search_languages=True)
+        query_lang = detect_language(query, threshold=0.55, only_search_languages=True)
         query_lang = query_lang or preferences.client.locale_tag or 'all'
 
     if not is_locked('categories') and raw_text_query.specific:
