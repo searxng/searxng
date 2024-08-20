e028d9e3a2f823cf1dc57ad1c0e95f59ff00bfe8
Markus Heiser <markus.heiser@darmarit.de>
2024-08-20 16:43:49 +0200

    [fix] debian/ubuntu python-is-python3
    Closes: https://github.com/searxng/searxng/issues/3235
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- utils/searxng.sh
+++ utils/searxng.sh
@@ -96,13 +96,8 @@ case $DIST_ID-$DIST_VERS in
         SEARXNG_BUILD_PACKAGES="${SEARXNG_BUILD_PACKAGES_debian}"
         APACHE_PACKAGES="$APACHE_PACKAGES libapache2-mod-proxy-uwsgi"
         ;;
-    ubuntu-20.04)
-        # https://wiki.ubuntu.com/FocalFossa/ReleaseNotes#Python3_by_default
-        SEARXNG_PACKAGES="${SEARXNG_PACKAGES_debian} python-is-python3"
-        SEARXNG_BUILD_PACKAGES="${SEARXNG_BUILD_PACKAGES_debian}"
-        ;;
     ubuntu-*|debian-*)
-        SEARXNG_PACKAGES="${SEARXNG_PACKAGES_debian}"
+        SEARXNG_PACKAGES="${SEARXNG_PACKAGES_debian} python-is-python3"
         SEARXNG_BUILD_PACKAGES="${SEARXNG_BUILD_PACKAGES_debian}"
         ;;
     arch-*)
@@ -453,6 +448,7 @@ searxng.install.clone() {
 
     # clone repo and add a safe.directory entry to git's system config / see
     # https://github.com/searxng/searxng/issues/1251
+    git config --system --add safe.directory "${REPO_ROOT}/.git"
     git_clone "$REPO_ROOT" "${SEARXNG_SRC}" \
               "$GIT_BRANCH" "${SERVICE_USER}"
     git config --system --add safe.directory "${SEARXNG_SRC}"
@@ -489,7 +485,7 @@ searxng.install.pyenv() {
     info_msg "create pyenv in ${SEARXNG_PYENV}"
     tee_stderr 0.1 <<EOF | sudo -H -u "${SERVICE_USER}" -i 2>&1 | prefix_stdout "$_service_prefix"
 rm -rf "${SEARXNG_PYENV}"
-python3 -m venv "${SEARXNG_PYENV}"
+python -m venv "${SEARXNG_PYENV}"
 grep -qFs -- 'source ${SEARXNG_PYENV}/bin/activate' ~/.profile \
   || echo 'source ${SEARXNG_PYENV}/bin/activate' >> ~/.profile
 EOF
