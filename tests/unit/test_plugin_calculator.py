b176323e89f87e52effc44ab0310fbf736efdc74
Markus Heiser <markus.heiser@darmarit.de>
2024-10-27 10:25:42 +0100

    [fix] calculator: use locale from UI (not from selected language)
    Closes: https://github.com/searxng/searxng/issues/3956
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- tests/unit/test_plugin_calculator.py
+++ tests/unit/test_plugin_calculator.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # pylint: disable=missing-module-docstring
 
-from mock import Mock
+import flask
 from parameterized.parameterized import parameterized
 from searx import plugins
+from searx import preferences
 from tests import SearxTestCase
 from .test_utils import random_string
 
@@ -11,63 +12,78 @@ from .test_plugins import get_search_mock
 
 
 class PluginCalculator(SearxTestCase):  # pylint: disable=missing-class-docstring
+
     def setUp(self):
+        from searx import webapp  # pylint: disable=import-outside-toplevel
+
+        self.webapp = webapp
         self.store = plugins.PluginStore()
         plugin = plugins.load_and_initialize_plugin('searx.plugins.calculator', False, (None, {}))
         self.store.register(plugin)
+        self.preferences = preferences.Preferences(["simple"], ["general"], {}, self.store)
+        self.preferences.parse_dict({"locale": "en"})
 
     def test_plugin_store_init(self):
         self.assertEqual(1, len(self.store.plugins))
 
     def test_single_page_number_true(self):
-        request = Mock(remote_addr='127.0.0.1')
-        search = get_search_mock(query=random_string(10), pageno=2)
-        result = self.store.call(self.store.plugins, 'post_search', request, search)
+        with self.webapp.app.test_request_context():
+            flask.request.preferences = self.preferences
+            search = get_search_mock(query=random_string(10), pageno=2)
+            result = self.store.call(self.store.plugins, 'post_search', flask.request, search)
+
         self.assertTrue(result)
         self.assertNotIn('calculate', search.result_container.answers)
 
     def test_long_query_true(self):
-        request = Mock(remote_addr='127.0.0.1')
-        search = get_search_mock(query=random_string(101), pageno=1)
-        result = self.store.call(self.store.plugins, 'post_search', request, search)
+        with self.webapp.app.test_request_context():
+            flask.request.preferences = self.preferences
+            search = get_search_mock(query=random_string(101), pageno=1)
+            result = self.store.call(self.store.plugins, 'post_search', flask.request, search)
+
         self.assertTrue(result)
         self.assertNotIn('calculate', search.result_container.answers)
 
     def test_alpha_true(self):
-        request = Mock(remote_addr='127.0.0.1')
-        search = get_search_mock(query=random_string(10), pageno=1)
-        result = self.store.call(self.store.plugins, 'post_search', request, search)
+        with self.webapp.app.test_request_context():
+            flask.request.preferences = self.preferences
+            search = get_search_mock(query=random_string(10), pageno=1)
+            result = self.store.call(self.store.plugins, 'post_search', flask.request, search)
+
         self.assertTrue(result)
         self.assertNotIn('calculate', search.result_container.answers)
 
     @parameterized.expand(
         [
-            ("1+1", "2", "en-US"),
-            ("1-1", "0", "en-US"),
-            ("1*1", "1", "en-US"),
-            ("1/1", "1", "en-US"),
-            ("1**1", "1", "en-US"),
-            ("1^1", "1", "en-US"),
-            ("1,000.0+1,000.0", "2,000", "en-US"),
-            ("1.0+1.0", "2", "en-US"),
-            ("1.0-1.0", "0", "en-US"),
-            ("1.0*1.0", "1", "en-US"),
-            ("1.0/1.0", "1", "en-US"),
-            ("1.0**1.0", "1", "en-US"),
-            ("1.0^1.0", "1", "en-US"),
-            ("1.000,0+1.000,0", "2.000", "de-DE"),
-            ("1,0+1,0", "2", "de-DE"),
-            ("1,0-1,0", "0", "de-DE"),
-            ("1,0*1,0", "1", "de-DE"),
-            ("1,0/1,0", "1", "de-DE"),
-            ("1,0**1,0", "1", "de-DE"),
-            ("1,0^1,0", "1", "de-DE"),
+            ("1+1", "2", "en"),
+            ("1-1", "0", "en"),
+            ("1*1", "1", "en"),
+            ("1/1", "1", "en"),
+            ("1**1", "1", "en"),
+            ("1^1", "1", "en"),
+            ("1,000.0+1,000.0", "2,000", "en"),
+            ("1.0+1.0", "2", "en"),
+            ("1.0-1.0", "0", "en"),
+            ("1.0*1.0", "1", "en"),
+            ("1.0/1.0", "1", "en"),
+            ("1.0**1.0", "1", "en"),
+            ("1.0^1.0", "1", "en"),
+            ("1.000,0+1.000,0", "2.000", "de"),
+            ("1,0+1,0", "2", "de"),
+            ("1,0-1,0", "0", "de"),
+            ("1,0*1,0", "1", "de"),
+            ("1,0/1,0", "1", "de"),
+            ("1,0**1,0", "1", "de"),
+            ("1,0^1,0", "1", "de"),
         ]
     )
     def test_localized_query(self, operation: str, contains_result: str, lang: str):
-        request = Mock(remote_addr='127.0.0.1')
-        search = get_search_mock(query=operation, lang=lang, pageno=1)
-        result = self.store.call(self.store.plugins, 'post_search', request, search)
+        with self.webapp.app.test_request_context():
+            self.preferences.parse_dict({"locale": lang})
+            flask.request.preferences = self.preferences
+            search = get_search_mock(query=operation, lang=lang, pageno=1)
+            result = self.store.call(self.store.plugins, 'post_search', flask.request, search)
+
         self.assertTrue(result)
         self.assertIn('calculate', search.result_container.answers)
         self.assertIn(contains_result, search.result_container.answers['calculate']['answer'])
@@ -78,8 +94,10 @@ class PluginCalculator(SearxTestCase):  # pylint: disable=missing-class-docstrin
         ]
     )
     def test_invalid_operations(self, operation):
-        request = Mock(remote_addr='127.0.0.1')
-        search = get_search_mock(query=operation, pageno=1)
-        result = self.store.call(self.store.plugins, 'post_search', request, search)
+        with self.webapp.app.test_request_context():
+            flask.request.preferences = self.preferences
+            search = get_search_mock(query=operation, pageno=1)
+            result = self.store.call(self.store.plugins, 'post_search', flask.request, search)
+
         self.assertTrue(result)
         self.assertNotIn('calculate', search.result_container.answers)
