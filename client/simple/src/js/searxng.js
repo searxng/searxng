14a2700a2e51b992cee88cbc16f7b10ab7c8d0b2
Markus Heiser <markus.heiser@darmarit.de>
2025-01-23 11:03:58 +0100

    [web-client] simple theme: build & development env based on vite.
    Vite is a build tool, it consists of two major parts [1]:
    
    - A dev server that provides rich feature enhancements over native ES modules,
      for example extremely fast Hot Module Replacement (HMR).
    
    - A build command that bundles your code with Rollup, pre-configured to output
      highly optimized static assets for production.
    
    [1] https://vite.dev/guide/
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

---
+++ client/simple/src/js/searxng.js
@@ -0,0 +1,7 @@
+import "./main/00_toolkit.js";
+import "./main/infinite_scroll.js";
+import "./main/keyboard.js";
+import "./main/mapresult.js";
+import "./main/preferences.js";
+import "./main/results.js";
+import "./main/search.js";
---
+++ utils/lib_sxng_vite.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+
+vite.help(){
+    cat <<EOF
+vite.:  .. to be done ..
+  simple.:
+    build: build static files of the simple theme
+    dev:   start development server
+EOF
+}
+
+VITE_SIMPLE_THEME="${REPO_ROOT}/client/simple"
+VITE_SIMPLE_DIST="${REPO_ROOT}/searx/static/themes/simple"
+
+vite.simple.dev() {
+
+    (   set -e
+        build_msg SIMPLE "start server for FE development of: ${VITE_SIMPLE_THEME}"
+        pushd "${VITE_SIMPLE_THEME}"
+        npm install
+        npm exec -- vite
+        popd &> /dev/null
+    )
+
+}
+
+vite.simple.build() {
+
+    # build static files of the simple theme
+
+    (   set -e
+        build_msg SIMPLE "run build of theme from: ${VITE_SIMPLE_THEME}"
+
+        pushd "${VITE_SIMPLE_THEME}"
+        npm install
+        npm run fix
+        npm run build
+
+    )
+}
