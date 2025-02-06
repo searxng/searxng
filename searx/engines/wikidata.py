147bda894e380b066f0d57dd7d3b1cb0974a0307
Markus Heiser <markus.heiser@darmarit.de>
2025-02-06 14:44:10 +0100

    [fix] Internet links disappeared from wikidata side box
    Closes: https://github.com/searxng/searxng/issues/4285
    
    Reported and tested by: Popolon
    Suggested-by: @dalf
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/engines/wikidata.py
+++ searx/engines/wikidata.py
@@ -641,7 +641,7 @@ class WDURLAttribute(WDAttribute):
             return get_external_url(url_id, value)
 
         if self.url_path_prefix:
-            [account, domain] = value.split('@')
+            [account, domain] = value.split('@', 1)
             return f"https://{domain}{self.url_path_prefix}{account}"
 
         return value
