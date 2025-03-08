1951a8bedd04080832bcebd6c710dbb7068273b9
Bnyro <bnyro@tutanota.com>
2025-03-08 11:42:17 +0000

    [doc] build from commit a88b4d7036002639ee09d01efb0279c87b8e23c3

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2025.3.8+a88b4d703',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
