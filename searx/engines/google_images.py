b0773a637468e8e185c3456f8ed7d7f23df39fd8
Markus Heiser <markus.heiser@darmarit.de>
2025-03-27 06:42:22 +0100

    [fix] make docs -> ERROR: Unknown target name: "google: max 50 pages".
    Fix the issues reported by sphinx build::
    
        docstring of searx.engines.google.max_page:1: ERROR: Unknown target name: "google: max 50 pages".
        docstring of searx.engines.google_images.max_page:1: ERROR: Unknown target name: "google: max 50 pages".
        docstring of searx.engines.google_scholar.max_page:1: ERROR: Unknown target name: "google: max 50 pages".
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/engines/google_images.py
+++ searx/engines/google_images.py
@@ -47,9 +47,9 @@ about = {
 categories = ['images', 'web']
 paging = True
 max_page = 50
-"""`Google: max 50 pages`_
+"""`Google max 50 pages`_
 
-.. _Google: max 50 pages: https://github.com/searxng/searxng/issues/2982
+.. _Google max 50 pages: https://github.com/searxng/searxng/issues/2982
 """
 
 time_range_support = True
