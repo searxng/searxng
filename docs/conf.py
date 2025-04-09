15384e8fc596da9c4a7e27393f8100018c3a61ed
Markus Heiser <markus.heiser@darmarit.de>
2025-04-09 16:36:50 +0200

    [fix] make docs - ERROR: Unknown target name: "auth_key"
    BTW: fix a bug with sys.path: repo-root (not util) needs to added to generate
    autodoc from scripts in ./searxng_extra
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- docs/conf.py
+++ docs/conf.py
@@ -161,7 +161,7 @@ issues_github_path = "searxng/searxng"
 notfound_urls_prefix = '/'
 
 sys.path.append(os.path.abspath('_themes'))
-sys.path.insert(0, os.path.abspath("../utils/"))
+sys.path.insert(0, os.path.abspath("../"))
 html_theme_path = ['_themes']
 html_theme = "searxng"
 
