71603a69d26c3e8ce5d55f1e066a06b32d2c77b1
Markus Heiser <markus.heiser@darmarit.de>
2025-08-10 11:37:01 +0200

    [fix] replace X-Scheme by X-Forwarded-Proto header
    The HTTP X-Forwarded-Proto (XFP) request header is a *de-facto* standard header
    for identifying the protocol (HTTP or HTTPS) that a client used to connect to a
    proxy or load balancer.[1]
    
    The ``X-Scheme`` header was added 10 years ago, why ``X-Scheme`` was used back
    then and not ``X-Forwarded-Proto``, nobody knows today / possibly because
    ``X-Forwarded-Proto`` wasn't a *de-facto* standard back then.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/X-Forwarded-Proto
    [2] https://github.com/searx/searx/commit/6ef7c3276

--- searx/flaskfix.py
+++ searx/flaskfix.py
@@ -21,7 +21,7 @@ class ReverseProxyPathFix:
         proxy_pass http://127.0.0.1:8000;
         proxy_set_header Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header X-Script-Name /myprefix;
         }
 
