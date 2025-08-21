cfb6a7d61cfe3a485591852d55600263274f1126
Markus Heiser <markus.heiser@darmarit.de>
2025-08-21 18:06:57 +0200

    [mod] add some missing type annotations & exports
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/network/raise_for_httperror.py
+++ searx/network/raise_for_httperror.py
@@ -3,6 +3,8 @@
 
 """
 
+from searx.extended_types import SXNG_Response
+
 from searx.exceptions import (
     SearxEngineCaptchaException,
     SearxEngineTooManyRequestsException,
@@ -11,7 +13,7 @@ from searx.exceptions import (
 from searx import get_setting
 
 
-def is_cloudflare_challenge(resp):
+def is_cloudflare_challenge(resp: SXNG_Response) -> bool:
     if resp.status_code in [429, 503]:
         if ('__cf_chl_jschl_tk__=' in resp.text) or (
             '/cdn-cgi/challenge-platform/' in resp.text
@@ -24,11 +26,11 @@ def is_cloudflare_challenge(resp):
     return False
 
 
-def is_cloudflare_firewall(resp):
+def is_cloudflare_firewall(resp: SXNG_Response) -> bool:
     return resp.status_code == 403 and '<span class="cf-error-code">1020</span>' in resp.text
 
 
-def raise_for_cloudflare_captcha(resp):
+def raise_for_cloudflare_captcha(resp: SXNG_Response) -> None:
     if resp.headers.get('Server', '').startswith('cloudflare'):
         if is_cloudflare_challenge(resp):
             # https://support.cloudflare.com/hc/en-us/articles/200170136-Understanding-Cloudflare-Challenge-Passage-Captcha-
@@ -44,19 +46,19 @@ def raise_for_cloudflare_captcha(resp):
             )
 
 
-def raise_for_recaptcha(resp):
+def raise_for_recaptcha(resp: SXNG_Response) -> None:
     if resp.status_code == 503 and '"https://www.google.com/recaptcha/' in resp.text:
         raise SearxEngineCaptchaException(
             message='ReCAPTCHA', suspended_time=get_setting('search.suspended_times.recaptcha_SearxEngineCaptcha')
         )
 
 
-def raise_for_captcha(resp):
+def raise_for_captcha(resp: SXNG_Response) -> None:
     raise_for_cloudflare_captcha(resp)
     raise_for_recaptcha(resp)
 
 
-def raise_for_httperror(resp):
+def raise_for_httperror(resp: SXNG_Response) -> None:
     """Raise exception for an HTTP response is an error.
 
     Args:
