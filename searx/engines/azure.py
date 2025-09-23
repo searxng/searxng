a83af73ec20148affa5bd91662d58f958b39f08a
Tommaso Colella <tommasocolella95@gmail.com>
2025-09-23 16:13:42 +0000

    [fix] engine: reformat azure engine, remove trailing spaces from settings.yml, change function param names

--- searx/engines/azure.py
+++ searx/engines/azure.py
@@ -49,58 +49,52 @@ about = {
 }
 
 
-def authenticate(tenant_id : str, client_id : str, client_secret : str) -> str:
+def authenticate(t_id: str, c_id: str, c_secret: str) -> str:
     """Authenticates to Azure using Oauth2 Client Credentials Flow and returns an access token"""
-    
-    url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+
+    url = f"https://login.microsoftonline.com/{t_id}/oauth2/v2.0/token"
     body = {
-        "client_id": client_id,
-        "client_secret": client_secret,
+        "client_id": c_id,
+        "client_secret": c_secret,
         "grant_type": "client_credentials",
-        "scope": "https://management.azure.com/.default"
+        "scope": "https://management.azure.com/.default",
     }
-    
-    resp : SXNG_Response=http_post(url, body)
+
+    resp: SXNG_Response = http_post(url, body)
     return resp.json()["access_token"]
 
 
 def request(query: str, params: "OnlineParams") -> None:
 
-    token=authenticate(tenant_id, client_id, client_secret)
+    token = authenticate(tenant_id, client_id, client_secret)
 
     params["url"] = batch_endpoint
     params["method"] = "POST"
     params["headers"]["Authorization"] = f"Bearer {token}"
-    params["headers"]["Content-Type"] = "application/json"  
+    params["headers"]["Content-Type"] = "application/json"
     params["json"] = {
         "requests": [
             {
                 "url": "/providers/Microsoft.ResourceGraph/resources?api-version=2024-04-01",
                 "httpMethod": "POST",
                 "name": "resourceGroups",
-                "requestHeaderDetails": {
-                    "commandName": "Microsoft.ResourceGraph"
-                },
+                "requestHeaderDetails": {"commandName": "Microsoft.ResourceGraph"},
                 "content": {
                     "query": f"ResourceContainers | where (name contains ('{query}'))|where (type =~ ('Microsoft.Resources/subscriptions/resourcegroups'))|project id,name,type,kind,subscriptionId,resourceGroup|extend matchscore = name startswith '{query}' | extend normalizedName = tolower(tostring(name)) | sort by matchscore desc, normalizedName asc | take 30"
-                }
+                },
             },
             {
                 "url": "/providers/Microsoft.ResourceGraph/resources?api-version=2024-04-01",
-                "httpMethod": "POST", 
+                "httpMethod": "POST",
                 "name": "resources",
-                "requestHeaderDetails": {
-                    "commandName": "Microsoft.ResourceGraph"
-                },
-                "content": {
-                    "query": f"Resources | where name contains '{query}' | take 30"
-                }
-            }
+                "requestHeaderDetails": {"commandName": "Microsoft.ResourceGraph"},
+                "content": {"query": f"Resources | where name contains '{query}' | take 30"},
+            },
         ]
     }
 
 
-def response(resp : "SXNG_Response") -> EngineResults:
+def response(resp: "SXNG_Response") -> EngineResults:
     res = EngineResults()
     json_data = resp.json()
     # print(json_data)
