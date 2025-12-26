4979884e108dc4d3a6c354fcf9ef6278f3432423
seres <seres@seres.eu.org>
2025-12-26 15:52:09 +0100

    formatting

--- searx/engines/mediawiki.py
+++ searx/engines/mediawiki.py
@@ -157,7 +157,10 @@ def response(resp):
         timestamp = result.get('timestamp')
 
         url = (
-            base_url.format(language=resp.search_params['language']) + content_path.rstrip('/') + ('/' if content_path.rstrip('/') else '') + quote(title.replace(' ', '_').encode())
+            base_url.format(language=resp.search_params['language'])
+            + content_path.rstrip('/')
+            + ('/' if content_path.rstrip('/') else '')
+            + quote(title.replace(' ', '_').encode())
         )
         if sectiontitle:
             # in case of sectiontitle create a link to the section in the wiki page
