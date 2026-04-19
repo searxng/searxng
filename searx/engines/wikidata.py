bf76a271948c8edc9d23f4c8fea1e8706e912bbd
Markus Heiser <markus.heiser@darmarit.de>
2026-04-19 09:35:13 +0200

    [fix] engine: wikidata - initialization fails with KeyError (#5993)
    The response to QUERY_PROPERTY_NAMES has changed; fields without the `name`
    field are now also returned.
    
    Closes: https://github.com/searxng/searxng/issues/5982
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/engines/wikidata.py
+++ searx/engines/wikidata.py
@@ -802,8 +802,11 @@ def init(engine_settings=None):  # pylint: disable=unused-argument
     query = QUERY_PROPERTY_NAMES.replace('%ATTRIBUTES%', " ".join(wikidata_property_names))
     jsonresponse = send_wikidata_query(query, timeout=20)
     for result in jsonresponse.get('results', {}).get('bindings', {}):
-        name = result['name']['value']
-        lang = result['name']['xml:lang']
+        name_field = result.get("name")
+        if not name_field:
+            continue
+        name = name_field["value"]
+        lang = name_field["xml:lang"]
         entity_id = result['item']['value'].replace('http://www.wikidata.org/entity/', '')
         WIKIDATA_PROPERTIES[(entity_id, lang)] = name.capitalize()
 
