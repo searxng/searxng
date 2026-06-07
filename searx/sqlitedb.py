b04bbebac1eb628a0d78f17c0623359842ce35a6
Markus Heiser <markus.heiser@darmarIT.de>
2026-06-07 08:22:19 +0200

    Revert "[fix] no such table during engine init (#6185)"
    This reverts commit 9d49a9f344f17b069ba8e6b6720db74991e7ca4b.

--- searx/sqlitedb.py
+++ searx/sqlitedb.py
@@ -327,6 +327,7 @@ class SQLiteAppl(abc.ABC):
 
         if self._init_done:
             return False
+        self._init_done = True
 
         logger.debug("init DB: %s", self.db_url)
         self.properties.init(conn)
@@ -341,7 +342,6 @@ class SQLiteAppl(abc.ABC):
                 raise sqlite3.DatabaseError("Expected DB schema v%s, DB schema is v%s" % (self.DB_SCHEMA, ver))
             logger.debug("DB_SCHEMA = %s", ver)
 
-        self._init_done = True
         return True
 
     def create_schema(self, conn: sqlite3.Connection):
@@ -368,9 +368,6 @@ class SQLiteProperties(SQLiteAppl):
          PRIMARY KEY (name))
 
     """
-
-    _locks: dict[str, threading.Lock] = {}
-    _locks_lock = threading.Lock()
 
     SQLITE_JOURNAL_MODE: str = "WAL"
 
@@ -409,16 +406,11 @@ CREATE TABLE IF NOT EXISTS properties (
 
         if self._init_done:
             return False
-        with self._locks_lock:
-            db_lock = self._locks.setdefault(self.db_url, threading.Lock())
-        with db_lock:
-            if self._init_done:
-                return False
-            logger.debug("init properties of DB: %s", self.db_url)
-            res = conn.execute(self.SQL_TABLE_EXISTS)
-            if res.fetchone() is None:  # DB schema needs to be be created
-                self.create_schema(conn)
-            self._init_done = True
+        self._init_done = True
+        logger.debug("init properties of DB: %s", self.db_url)
+        res = conn.execute(self.SQL_TABLE_EXISTS)
+        if res.fetchone() is None:  # DB schema needs to be be created
+            self.create_schema(conn)
         return True
 
     def __call__(self, name: str, default: t.Any = None) -> t.Any:
