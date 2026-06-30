7397ae7d7d9b8db1882b21c064ff41cdd292472a
Markus Heiser <markus.heiser@darmarit.de>
2026-06-30 09:08:14 +0200

    WIP

--- searx/engines/uruky.py
+++ searx/engines/uruky.py
@@ -25,6 +25,7 @@ import typing as t
 from urllib.parse import urlencode
 
 from searx.result_types import EngineResults
+from searx.exceptions import SearxEngineAccessDeniedException
 
 if t.TYPE_CHECKING:
     from searx.search.processors import OnlineParams
@@ -36,33 +37,63 @@ about = {
     "use_official_api": True,
     "require_api_key": True,
     "results": "JSON",
+    "description": (
+        "Uruky is a EU-based, private search engine, free from ads and tracking,"
+        " with a focus on search personalization, not AI or side-products."
+    ),
 }
 
 api_key = ""
-uruky_categ: t.Literal["search", "images"] = "search"
-"""Available categories: "search", "images"."""
-uruky_providers = []
-categories = ["general", "web"]
+categories = None
 paging = True
 safesearch = True
 time_range_support = True
 
 base_url = "https://uruky.com"
+safe_search_map = {0: "0", 1: "1", 2: "1"}
+time_range_map = {"day": "d", "week": "w", "month": "m", "year": "y"}
 
-categ_paths = {
+uruky_categ: "UrukyCategory" = None  # type: ignore
+uruky_providers: "list[UrukyProvider]" = []
+"""List of providers, the defaults are (:py:obj:`URUKY_CATEGORIES`):
+
+- ``search``: mojeek, eusp, linkup, marginalia, serper, prieco
+- ``images``: pixabay, serper
+"""
+
+UrukyCategory: t.TypeAlias = t.Literal["search", "images"]
+UrukyProvider: t.TypeAlias = t.Literal[
+    "mojeek",
+    "eusp",
+    "linkup",
+    "marginalia",
+    "serper",
+    "prieco",
+    "pixabay",
+    "serper",
+]
+
+CATEG_PATHS = {
     "search": "/search",
     "images": "/image-search",
 }
 
-safe_search_map = {0: "0", 1: "1", 2: "1"}
-time_range_map = {"day": "d", "week": "w", "month": "m", "year": "y"}
+URUKY_CATEGORIES: tuple[UrukyCategory] = t.get_args(UrukyCategory)
+"""Available categories."""
+
+URUKY_PROVIDERS: tuple[UrukyProvider] = t.get_args(UrukyProvider)
+"""Available providers."""
 
 
-def init(_):
+def setup(_):
     if not api_key:
-        raise ValueError("api_key is required for uruky")
-    if uruky_categ not in categ_paths:
+       raise ValueError("api_key is required for uruky")
+    if uruky_categ not in URUKY_CATEGORIES:
         raise ValueError(f"unsupported uruky_categ: {uruky_categ}")
+    for provider in uruky_providers:
+        if provider not in URUKY_PROVIDERS:
+            raise ValueError(f"unsupported provider in uruky_providers: {provider}")
+    return True
 
 
 def request(query: str, params: "OnlineParams"):
@@ -87,13 +118,21 @@ def request(query: str, params: "OnlineParams"):
     if uruky_providers:
         args["sp"] = ",".join(uruky_providers)
 
-    params["url"] = f"{base_url}{categ_paths[uruky_categ]}?{urlencode(args)}"
+    params["method"] = "GET"
+    params["url"] = f"{base_url}{CATEG_PATHS[uruky_categ]}?{urlencode(args)}"
 
 
 def response(resp: "SXNG_Response") -> EngineResults:
+
+    if resp.status_code == 302:
+        # migth a problem with the login
+        raise SearxEngineAccessDeniedException(message=f"HTTP 302 {resp.text}: {resp.headers['location'][:30]}...")
+
     res = EngineResults()
+    items: list[dict[str, str]] = resp.json().get("results", [])  # type: ignore
+
     if uruky_categ == "search":
-        for r in resp.json().get("results", []):
+        for r in items:
             res.add(
                 res.types.MainResult(
                     url=r["url"],
@@ -101,8 +140,9 @@ def response(resp: "SXNG_Response") -> EngineResults:
                     content=r.get("description", ""),
                 )
             )
+
     elif uruky_categ == "images":
-        for r in resp.json().get("results", []):
+        for r in items:
             res.add(
                 res.types.Image(
                     url=r["sourceUrl"],
