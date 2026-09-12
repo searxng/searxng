ab9eb3ea9661e4e8d45c79e42f701b92decefaa8
Markus Heiser <markus.heiser@darmarit.de>
2026-09-12 14:37:03 +0200

    [mod] openverse - use of internal APIs and addition result fields (#6701)
    Additional fields:
      add resolution, thumbnail and author to the image result
    
    Types:
      use EngineResults and add type hints from internall APIs
    
    Disabled engine:
      as of 09/2026: The availability of https://openverse.org/ is poor, and queries
      and image requests often time out. However, these issues also occur when using
      a WEB browser to search on openverse.org or view images.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/engines/openverse.py
+++ searx/engines/openverse.py
@@ -1,54 +1,64 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+"""Openverse (formerly known as: Creative Commons search engine) [Images]
+
+As of 09/2026: The availability of https://openverse.org/ is poor, and queries
+and image requests often time out. However, these issues also occur when using a
+WEB browser to search on openverse.org or view images.
 """
 
-Openverse (formerly known as: Creative Commons search engine) [Images]
-
-"""
-
-from json import loads
+import typing as t
 from urllib.parse import urlencode
+from dateutil import parser
+
+from searx.result_types import EngineResults
+
+
+if t.TYPE_CHECKING:
+    from searx.extended_types import SXNG_Response
+    from searx.search.processors import OnlineParams
 
 
 about = {
-    "website": 'https://openverse.org/',
+    "website": "https://openverse.org/",
     "wikidata_id": None,
-    "official_api_documentation": 'https://api.openverse.org/v1/',
+    "official_api_documentation": "https://api.openverse.org/v1/",
     "use_official_api": True,
     "require_api_key": False,
-    "results": 'JSON',
+    "results": "JSON",
 }
 
-categories = ['images']
+categories = ["images"]
 
 paging = True
 page_size = 20
 
-base_url = 'https://api.openverse.org/v1/images/'
-search_string = '?page={page}&page_size={page_size}&format=json&{query}'
+base_url = "https://api.openverse.org/v1/images/"
 
 
-def request(query, params):
+def request(query: str, params: "OnlineParams"):
+    args: dict[str, str | int] = {
+        "q": query,
+        "page": params["pageno"],
+        "page_size": page_size,
+        "format": "json",
+    }
+    params["url"] = f"{base_url}?{urlencode(args)}"
 
-    search_path = search_string.format(query=urlencode({'q': query}), page_size=page_size, page=params['pageno'])
 
-    params['url'] = base_url + search_path
+def response(resp: "SXNG_Response") -> EngineResults:
+    res = EngineResults()
+    json_data = resp.json()  # type: ignore
 
-    return params
-
-
-def response(resp):
-    results = []
-
-    json_data = loads(resp.text)
-
-    for result in json_data['results']:
-        results.append(
-            {
-                'url': result['foreign_landing_url'],
-                'title': result['title'],
-                'img_src': result['url'],
-                'template': 'images.html',
-            }
+    for result in json_data["results"]:  # pyright: ignore[reportUnknownVariableType]
+        res.add(
+            res.types.Image(
+                url=result["foreign_landing_url"],
+                title=result["title"],
+                img_src=result["url"],
+                thumbnail_src=result["thumbnail"],
+                resolution=f"{result['width']} x {result['height']}",
+                author=result["creator"],
+                publishedDate=parser.parse(result["indexed_on"]),
+            )
         )
-
-    return results
+    return res
