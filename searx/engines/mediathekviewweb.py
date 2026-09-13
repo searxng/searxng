e5f0d8edcc2644fb11b4bcc7a58fbf9bf4c5e590
Bnyro <bnyro@tutanota.com>
2026-09-13 13:36:51 +0200

    [fix] mediathekviewweb: crashes when result has no hd video url  (#6717)

--- searx/engines/mediathekviewweb.py
+++ searx/engines/mediathekviewweb.py
@@ -61,7 +61,9 @@ def response(resp: "SXNG_Response") -> EngineResults:
     for item in mwv_result_list:
         item["hms"] = str(datetime.timedelta(seconds=item["duration"]))
 
-        video_url = item["url_video_hd"]
+        video_url = item["url_video_hd"] or item["url_video"] or item["url_video_low"] or item["url_video"]
+        if not video_url:
+            continue
 
         res.add(
             res.types.LegacyResult(
