6c3258c2b253fcae9f128be6f5e3bf29f0b0c9ec
Bnyro <bnyro@tutanota.com>
2026-09-17 17:50:19 +0200

    [fix] 500px: crashes when there's no description

--- searx/engines/500px.py
+++ searx/engines/500px.py
@@ -143,7 +143,7 @@ def response(resp: "SXNG_Response"):
                 thumbnail_src=image_urls[0],
                 img_src=image_urls[-1],
                 title=node["title"],
-                content=node["description"],
+                content=node["description"] or "",
                 author=node["uploader"]["displayName"],
                 resolution=f"{node['width']}x{node['height']}",
             )
